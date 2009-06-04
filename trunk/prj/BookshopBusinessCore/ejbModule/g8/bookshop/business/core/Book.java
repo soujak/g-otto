aa46a235b274af3c6a08fc5e8cfed47c51ac3c0e
soujak <soujak@a791df2a-2387-11de-9edc-3f40a397ab60>
2009-06-04 15:35:05 +0000

    Aggiunta delle interfacce locali delle ejb.

--- trunk/prj/BookshopBusinessCore/ejbModule/g8/bookshop/business/core/Book.java
+++ trunk/prj/BookshopBusinessCore/ejbModule/g8/bookshop/business/core/Book.java
@@ -7,8 +7,11 @@ package g8.bookshop.business.core;
  * @author soujak
  *
  */
-public class Book {
+public class Book implements BookLocal {
 
+	/* (non-Javadoc)
+	 * @see g8.bookshop.business.core.BookLocal#getId()
+	 */
 	public String getId() {
 		// TODO
 		return null;
