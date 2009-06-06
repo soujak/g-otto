f90142ce904104cbb6e9e8a7d9f254db1d9c9606
shigure.minazuki@gmail.com <shigure.minazuki@gmail.com>
2009-06-06 09:10:34 +0000

    Una prima versione "acerba" con un css e qualche jsp e jspf, e la bozza delle servlet che saranno..

---
+++ prj/BookshopPresentation/src/g8/bookshop/presentation/servlet/usermanager/Disconnect.java
@@ -0,0 +1,36 @@
+package g8.bookshop.presentation.servlet.usermanager;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Disconnect extends HttpServlet {
+
+	public Disconnect() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
