50412191617f7f75766f20194f0554fbda4e0bdd
fdraicchio <fdraicchio@a791df2a-2387-11de-9edc-3f40a397ab60>
2009-06-12 09:55:45 +0000

    aggiornamento client ws

---
+++ trunk/prj/BookshopPresentationUI/src/g8/bookshop/business/ws/CatalogueService.java
@@ -0,0 +1,13 @@
+/**
+ * CatalogueService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package g8.bookshop.business.ws;
+
+public interface CatalogueService extends java.rmi.Remote {
+    public java.lang.String fullSearch(java.lang.String arg0) throws java.rmi.RemoteException;
+    public java.lang.String search(java.lang.String arg0) throws java.rmi.RemoteException;
+}
---
+++ trunk/prj/BookshopPresentationUI/src/g8/bookshop/business/ws/CatalogueServiceService.java
@@ -0,0 +1,16 @@
+/**
+ * CatalogueServiceService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package g8.bookshop.business.ws;
+
+public interface CatalogueServiceService extends javax.xml.rpc.Service {
+    public java.lang.String getCatalogueServicePortAddress();
+
+    public g8.bookshop.business.ws.CatalogueService getCatalogueServicePort() throws javax.xml.rpc.ServiceException;
+
+    public g8.bookshop.business.ws.CatalogueService getCatalogueServicePort(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
