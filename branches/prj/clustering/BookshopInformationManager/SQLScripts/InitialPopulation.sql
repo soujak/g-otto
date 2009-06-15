2afe6c4ca9a0a35d3784fb4f90677437cb11967a
soujak <soujak@a791df2a-2387-11de-9edc-3f40a397ab60>
2009-06-15 16:52:53 +0000

    Divisione progetti business (incompleta).

--- branches/prj/clustering/BookshopInformationManager/SQLScripts/InitialPopulation.sql
+++ branches/prj/clustering/BookshopInformationManager/SQLScripts/InitialPopulation.sql
@@ -1,5 +1,8 @@
+delete from Book;
+delete from Credential;
 insert into Book set Id=1, Author='autore1', Editor='editore1', ISBN='isbn1', Title='titolo1', Year=0001;
 insert into Book set Id=2, Author='autore2', Editor='editore2', ISBN='isbn2', Title='titolo2', Year=0002;
 insert into Book set Id=3, Author='autore3', Editor='editore3', ISBN='isbn3', Title='titolo3', Year=0003;
 insert into Book set Id=4, Author='autore4', Editor='editore4', ISBN='isbn4', Title='titolo4', Year=0004;
 insert into Credential set Name='gnappo', Password='gnappo';
+insert into Credential set Name='soujak', Password='soujak';
