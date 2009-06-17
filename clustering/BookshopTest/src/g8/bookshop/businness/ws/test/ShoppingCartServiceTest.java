a5c62dfbb3036ca5068707a1e3152719956dcb58
Marco Solieri <ms@xt3.it>
2009-06-17 21:54:22 +0000

    Business/Clustering: impostazione delle unita` di collaudo

--- clustering/BookshopTest/src/g8/bookshop/businness/ws/test/ShoppingCartServiceTest.java
+++ clustering/BookshopTest/src/g8/bookshop/businness/ws/test/ShoppingCartServiceTest.java
@@ -40,9 +40,8 @@ public class ShoppingCartServiceTest {
 		Properties env = new Properties();
 		env.setProperty(Context.INITIAL_CONTEXT_FACTORY,
 				"org.jnp.interfaces.NamingContextFactory");
-		env.setProperty(Context.PROVIDER_URL, "jnp://localhost:1099");
-		env.setProperty(Context.URL_PKG_PREFIXES,
-				"org.jboss.naming:org.jnp.interfaces");
+		env.setProperty("jnp.partitionName", "G8Business");
+		env.setProperty(Context.URL_PKG_PREFIXES,"org.jboss.naming:org.jnp.interfaces");
 		ctx = new InitialContext(env);
 		sc = (ShoppingCartServiceRemote) ctx
 				.lookup("BookshopBusinessWS/ShoppingCartService/remote");
