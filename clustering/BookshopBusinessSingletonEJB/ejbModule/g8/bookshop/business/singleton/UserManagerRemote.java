2e8f46ee550feab143b306bddaceaa8da1f72b81
Marco Solieri <ms@xt3.it>
2009-06-18 13:06:20 +0000

    Business/Clustering: replicazione core

--- clustering/BookshopBusinessSingletonEJB/ejbModule/g8/bookshop/business/singleton/UserManagerRemote.java
+++ clustering/BookshopBusinessSingletonEJB/ejbModule/g8/bookshop/business/singleton/UserManagerRemote.java
@@ -5,6 +5,7 @@ import g8.bookshop.business.core.GuestRemote;
 import g8.bookshop.business.core.UserRemote;
 
 import javax.ejb.Remote;
+import javax.naming.NamingException;
 
 /**
  * Remote interface UserManager
@@ -24,8 +25,9 @@ public interface UserManagerRemote {
 	 * @param id user id.
 	 * @return User to which the specified id is mapped,
 	 * 		if id is not mapped create a new guest with the given id. 
+	 * @throws NamingException 
 	 */
-	public UserRemote getUser(String id);
+	public UserRemote getUser(String id) throws NamingException;
 	
 	/**
 	 * Authenticate a customer in the bookshop, verifying his credentials 
@@ -33,8 +35,9 @@ public interface UserManagerRemote {
 	 * @param n Guest's name
 	 * @param p Guest's password
 	 * @return true if the guest is successfully authenticated, false otherwise
+	 * @throws NamingException 
 	 */
-	public boolean authenticate(GuestRemote g, String n, String p);
+	public boolean authenticate(GuestRemote g, String n, String p) throws NamingException;
 	
 	/**
 	 * Disconnect a given customer from the bookshop
