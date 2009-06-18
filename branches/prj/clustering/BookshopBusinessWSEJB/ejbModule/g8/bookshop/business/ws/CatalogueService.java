efb8fc7a64caf7a06490bd8c3a0a20fd990e0cec
andrea.rappini <andrea.rappini@a791df2a-2387-11de-9edc-3f40a397ab60>
2009-06-18 14:41:57 +0000

    Tolta notazione clustered per i WS.

--- branches/prj/clustering/BookshopBusinessWSEJB/ejbModule/g8/bookshop/business/ws/CatalogueService.java
+++ branches/prj/clustering/BookshopBusinessWSEJB/ejbModule/g8/bookshop/business/ws/CatalogueService.java
@@ -30,7 +30,6 @@ import org.jboss.ejb3.annotation.Depends;
  */
 @Depends(value="ear=BookshopBusinessCore.ear,jar=BookshopBusinessCoreEJB.jar,name=Converter,service=EJB3")
 @Stateless
-@Clustered
 @WebService
 public class CatalogueService implements CatalogueServiceRemote {
 	
