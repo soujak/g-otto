2fdcf89abf34a07a91448d3bafd6e6b729fbec04
soujak <soujak@a791df2a-2387-11de-9edc-3f40a397ab60>
2009-06-24 17:00:04 +0000

    Business/Clustering: clusterizzazione user manager

---
+++ branches/prj/clustering/BookshopBusinessSingletonEJB/ejbModule/g8/bookshop/business/um/UserManagerMBean.java
@@ -0,0 +1,70 @@
+package g8.bookshop.business.um;
+
+import g8.bookshop.business.core.CustomerRemote;
+import g8.bookshop.business.core.GuestRemote;
+import g8.bookshop.business.core.UserRemote;
+
+import org.jboss.ejb3.annotation.Management;
+
+@Management
+public interface UserManagerMBean {
+	/**
+	 * Lookup for a user in the bookshop from his id
+	 * @param id user id
+	 * @return user to which the specified id is mapped, or null if id does not exists 
+	 */
+	public UserRemote lookup(String id);
+	/**
+	 * Search a user in the bookshop from his id (possibly new) 
+	 * @param id user id.
+	 * @return User to which the specified id is mapped,
+	 * 		if id is not mapped create a new guest with the given id. 
+	 * @throws NamingException 
+	 */
+	public UserRemote getUser(String id);
+	/**
+	 * Authenticate a customer in the bookshop, verifying his credentials 
+	 * @param g Guest to authenticate
+	 * @param n Guest's name
+	 * @param p Guest's password
+	 * @return true if the guest is successfully authenticated, false otherwise
+	 * @throws NamingException 
+	 */
+	public boolean authenticate(GuestRemote g, String n, String p);
+	/**
+	 * Disconnect a given customer from the bookshop
+	 * @param customerRemote Customer to disconnect
+	 * @return true if the customer is successfully disconnected, false otherwise
+	 */
+	public boolean disconnect(CustomerRemote customerRemote);
+	
+	/**
+	 * @throws Exception
+	 */
+	void create() throws Exception;
+	/**
+	 * @throws Exception
+	 */
+	void start() throws Exception;
+	/**
+	 * 
+	 */
+	void stop();
+	/**
+	 * 
+	 */
+	void destroy();
+	
+	/**
+	 * @return
+	 */
+	boolean isMasterNode();
+	/**
+	 * 
+	 */
+	void startSingleton();
+	/**
+	 * 
+	 */
+	void stopSingleton();
+}
